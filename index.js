5710905913ca7a5d528b63f97c768f8c89552687
TonyCipriani4 <ghaith.mohammed.ga@gmail.com>
2026-05-28 12:54:06 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+const publicPath = path.join(__dirname, "public");
+
+app.use(express.static(publicPath));
+
+app.get("/", (req, res) => {
+  res.send("OK");
+});
+app.get("/api/localize/*", (req, res) => {
+    res.sendFile(path.join(publicPath, `./api/localize/${req.params[0]}.json`));
+});
+const port = 3000;
+app.listen(port, '0.0.0.0', () => {
+  console.log(`X-Ray Is Starting 90 ${port}`);
+});
